1eba4fbacaeb9dd5b956cf0dca06c73e5c7a139c
lybamughees <66569488+lybamughees@users.noreply.github.com>
2024-01-23 12:16:49 -0500

    Add files via upload

--- modules/Medication-Alarm/Medication-Alarm.js
+++ modules/Medication-Alarm/Medication-Alarm.js
@@ -1,7 +1,7 @@
 /* global Module */
 
 /* Magic Mirror
- * Module: Medication-Alarm_module
+ * Module: Medication-Alarm
  *
  * By Lyba Mughees
  * MIT Licensed.
@@ -12,12 +12,59 @@
 
 /* global Module, Log */
 
+
 Module.register("Medication-Alarm", {
-  defaults: {},
+  defaults: {
+    medicationSchedule: []
+  },
 
   start: function () {
     Log.info("Medication-Alarm module started...");
+    this.scheduleMedicationAlarms();
   },
 
+  scheduleMedicationAlarms: function () {
+    setInterval(() => {
+      const now = new Date();
+      const currentTime = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`;
+
+ 
+      const scheduledMedications = this.config.medicationSchedule.filter((medication) => {
+        return (
+          medication.day === now.toLocaleDateString() &&
+          medication.time === currentTime
+        );
+      });
+
+      if (scheduledMedications.length > 0) {
+        const medication = scheduledMedications[0];
+        const message = `It's time for your medication: ${medication.brand_name} at ${medication.time}`;
+        this.showNotification(message);
+        console.log(message);
+      }
+    }, 60000); 
+    
+  },
+
+  getStyles: function () {
+    return [
+        "Medication-Alarm.css" 
+    ];
+},
+
+
+showNotification: function (message) {
+  const notificationElement = document.createElement("div");
+  notificationElement.className = "popup-div";
+  notificationElement.innerHTML = `<span>${message}</span>`;
+
+  
+  document.body.appendChild(notificationElement);
+
+  
+  setTimeout(() => {
+      notificationElement.remove();
+  }, 10000);
+}
 
 });
--- modules/Medication-Alarm/node_helper.js
+++ modules/Medication-Alarm/node_helper.js
@@ -6,14 +6,27 @@
  */
 
 const NodeHelper = require("node_helper");
-const schedule = require("node-schedule");
 const sqlite3 = require("sqlite3").verbose();
 
 module.exports = NodeHelper.create({
   start: function () {
-    console.log("Medication-Alarm helper started...");
-
+    console.log("Medication Alarm helper started...");
+    this.config = {}; 
+    this.fetchMedicationSchedule();
   },
 
-  
+  fetchMedicationSchedule: function () {
+    const db = new sqlite3.Database("modules/Medication-Management/medications.db");
+
+    db.all("SELECT * FROM medication_schedule", [], (err, rows) => {
+      if (err) {
+        console.error("Error fetching medication schedule:", err);
+        db.close();
+        return;
+      }
+
+      this.config.medicationSchedule = rows;
+      db.close();
+    });
+  },
 });
